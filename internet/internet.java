76b5863b1ac3974085d15ddd0d0b4f83d3f84855
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2021-03-04 17:17:39 +0100

    Updated internet.java

--- internet/internet.java
+++ internet/internet.java
@@ -5,8 +5,16 @@ import java.net.*;
 
 // internet class
 
-public class internet() {
+public class Internet {
     
-    
+    public class void internet() {
+        
+        InetAddress localAddress = InetAddress.getLocalHost();
+        URL tcpServer = new URL("tcp://127.0.0.1:" +tcpServerPort +"");
+        URLConnection tcpServerConnection = tcpServer.openConnection();
+        InputStream tcpServerInput = tcpServerConnection.getInputStream();
+        int datas = tcpServerConnection.getContentLenght();
+        
+    }
     
 }
