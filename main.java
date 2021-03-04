29f195810a411dc4f814a9105319235c4056af2f
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2021-03-04 14:20:25 +0100

    Updated main.java

--- main.java
+++ main.java
@@ -10,8 +10,8 @@ public class mainClassDatas {
     
     public static void pools(String[] args) {
         
-        public static bool bitcoinPoolEnabled = false;
-        public static bool marscoinPoolEnabled = true;
+        int bitcoinPoolEnabled = 0;
+        int marscoinPoolEnabled = 1;
         
     }
     
@@ -37,12 +37,12 @@ public class Main {
     
     public static void main(String[] args) {
         
-        if (mainClassDatas.pools.bitcoinPoolEnabled = true) {
+        if (mainClassDatas.pools.bitcoinPoolEnabled = 1) {
             
             System.out.println("The bitcoin pool is starting !");
             
         };
-        if (mainClassDatas.pools.marscoinPoolEnabled = true) {
+        if (mainClassDatas.pools.marscoinPoolEnabled = 1) {
             
             System.out.println("The marscoin pool is starting !");
             
