4080ddc3d219d3c80fa3595e248bab127737e2d5
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2021-03-04 14:07:38 +0100

    Created main.java

---
+++ main.java
@@ -0,0 +1,29 @@
+
+// default imports
+
+
+
+
+// main class
+
+public class Main {
+    
+    public static void gui(String[] args) {
+        
+        
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+    public static void pool(String[] args) {
+        
+        
+        
+    }
+    
+}
