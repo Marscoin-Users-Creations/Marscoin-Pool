3d23d0ab14f9cf144425b33ae3bb0d9adf7af14a
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2021-03-04 17:23:03 +0100

    Updated internet.java

--- internet/internet.java
+++ internet/internet.java
@@ -1,20 +1,24 @@
 // default imports
-
-import java.io.*;
 import java.net.*;
 
 
 // internet class
 
-public class Internet {
+public class internetDatas{
+    
+    public static void datas() {
+        
+        int tcpServerPort = 8080;
+        
+    }
+    
+}
+public class Internet{
     
     public class void internet() {
         
-        InetAddress localAddress = InetAddress.getLocalHost();
-        URL tcpServer = new URL("tcp://127.0.0.1:" +tcpServerPort +"");
-        URLConnection tcpServerConnection = tcpServer.openConnection();
-        InputStream tcpServerInput = tcpServerConnection.getInputStream();
-        int datas = tcpServerConnection.getContentLenght();
+        ServerSocket tcpServer = new ServerSocket(internetDatas.datas.tcpServerPort);
+        InputStream tcpServerStream = tcpServer.getInputStream();
         
     }
     
