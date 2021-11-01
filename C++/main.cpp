b22270b7f3c508b094bfbb62072caf37ee02ae1b
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2021-11-01 16:05:45 +0100

    Created main.cpp

---
+++ C++/main.cpp
@@ -0,0 +1,25 @@
+// default includes
+
+#include <fstream>
+#include <iostream>
+#include <std>
+
+#include "block/block.cpp"
+#include "blockchain/blockchain.cpp"
+#include "firewall/firewall.cpp"
+#include "internet/internet.cpp"
+
+
+// main functions and vars
+
+int walletAddress = "setWalletAdressHere"; // set the wallet address here like this : 127.0.0.1
+int walletPort = 0; // set the coin wallet port here
+
+
+// main namespace
+
+namespace main() {
+    
+    
+    
+};
